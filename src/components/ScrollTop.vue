e2170783555d63c949bfbd65c446d7ed61e535f7
xue <colpp61109@gmail.com>
2023-11-20 13:46:02 +0800

    mobile 4

--- src/components/ScrollTop.vue
+++ src/components/ScrollTop.vue
@@ -1,5 +1,5 @@
 <template>
-    <button type="button" class="btn btn-info position-absolute position-fixed ms-auto gotop-btn p-2" ref="gotopbtn" 
+    <button type="button" class="d-none d-sm-block btn btn-info position-absolute position-fixed ms-auto gotop-btn p-2" ref="gotopbtn" 
     @click.stop="gotoTopScroll" :class="height < 200 ? 'd-none' : ''"><i class="fa-regular fa-circle-up fs-2"></i></button>
 </template>
 
